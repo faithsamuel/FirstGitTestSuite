74d94ba3e5925b7ac6a8e5b5013c9615863c6ede
User <you@example.com>
2020-07-01 17:17:21 +0100

    adding LifeChanger website test suite

---
+++ Test Suites/LifeChangerWebsiteTestSuite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LifeChangerWebsiteTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>46f92047-21b6-4fd9-8297-52cfeeca574a</testSuiteGuid>
+</TestSuiteEntity>
